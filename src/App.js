c03796c7907db4dbbc33c031e990b408a112cfc1
uwdjany <djanatiuwase@gmail.com>
2023-01-20 19:25:50 +0200

    skills page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './component/navBar';
 import Home from './view/home';
 import AboutPage from './view/aboutMe';
 import SkillsPage from './view/mySkills';
+import ContactPage from './view/contact';
 
 
 function App() {
@@ -21,6 +22,9 @@ function App() {
   </div>
   <div>
   <SkillsPage/>
+  </div>
+  <div>
+  <ContactPage/>
   </div>
 
  
---
+++ src/view/contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div className=' bg-pageColor'>
+    <div>
+    <h1>Contact Me</h1>
+    </div>
+    <div>
+    <h2>Ways to Contact Me</h2>
+    </div>
+    
+    </div>
+  )
+}
+
+export default ContactPage
